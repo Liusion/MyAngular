728883382bd922063b585e63e09dc57d337e545a
unknown <283565505@qq.com>
2017-05-22 08:27:09 +0800

    create scope.js

---
+++ src/scope.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 28356 on 2017/05/21.
+ */
+function Scope() {
+    
+}
---
+++ test/scope_spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 28356 on 2017/05/21.
+ */
+describe("Scope",function () {
+    it("can be constructed and used as an object",function () {
+        var scope=new Scope();
+        scope.aProperty=1;
+        expect(scope.aProperty).toBe(1);
+    })
+});
