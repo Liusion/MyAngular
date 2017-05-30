fce3f9fa134b228003ff87363173941b35e76d3a
unknown <283565505@qq.com>
2017-05-30 16:23:28 +0800

    作用域和Digest（二）

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,5 +1,6 @@
 'use strict';
 var Scope = require('../src/scope');
+var _=require('lodash');
 describe("Scope", function() {
     it("can be constructed and used as an object", function() {
         var scope = new Scope();
@@ -13,9 +14,7 @@ describe("Scope", function() {
             scope=new Scope();
         });
         it("calls the listener function of a watch on first $digest",function () {
-            var watchFn =function () {
-                return "wat";
-            };
+            var watchFn =function () {return "wat";};
             var listenerFn=jasmine.createSpy();
             scope.$watch(watchFn,listenerFn);
             scope.$digest();
@@ -23,12 +22,10 @@ describe("Scope", function() {
         });
         it("calls the watch function with the scope as the argument",function () {
             var watchFn=jasmine.createSpy();
-            var listenFn=function () {
-                scope.$watchFn=function (watchFn, listenFn) {
-                    scope.digest();
-                    expect(watchFn).toHaveBeenCalledWith(scope);
-                }
-            };
+            var listenerFn=function(){};
+            scope.$watch(watchFn,listenerFn);
+            scope.$digest();
+            expect(watchFn).toHaveBeenCalledWith(scope);
         });
         it("calls the listener function when the watched value changes",function () {
             scope.someValue="a";
@@ -47,7 +44,7 @@ describe("Scope", function() {
             expect(scope.counter).toBe(2);
 
         });
-        it("calls the listener when watch value is first undefined",function () {
+        it("calls listener when watch value is first undefined",function () {
             scope.counter=0;
             scope.$watch(function (scope) {
                 return scope.someValue;
@@ -94,6 +91,89 @@ describe("Scope", function() {
             scope.name="Bob";
             scope.$digest();
             expect(scope.initial).toBe("B.")
+        });
+        it("gives up on the watchers after 10 iterations",function () {
+            scope.counterA=0;
+            scope.counterB=0;
+            scope.$watch(
+                function (scope) {return scope.counterA},
+                function (newValue, oldValue, scope) {
+                    scope.counterB++;
+                }
+            );
+            scope.$watch(
+                function (scope) {return scope.counterB},
+                function (newValue, oldValue, scope) {
+                    scope.counterA++;
+                }
+            );
+            expect(function () {
+                scope.$digest();
+            }).toThrow();
+        });
+        it("ends the digest when the last watch is clean",function () {
+            scope.array = _.range(100);
+            var watchExecutions=0;
+            _.times(100,function (i) {
+                scope.$watch(
+                    function (scope) {
+                        watchExecutions++;
+                        return scope.array[i];
+                    },
+                    function (newValue, oldValue, scope) {
+
+                    }
+                );
+            });
+            scope.$digest();
+            expect(watchExecutions).toBe(200);
+
+            scope.array[0]=420;
+            scope.$digest();
+            expect(watchExecutions).toBe(301);
+        });
+        it("does not end digest so that new watches are not run",function () {
+            scope.aValue="abc";
+            scope.counter=0;
+            scope.$watch(
+                function (scope) {return scope.aValue;},
+                function (newValue, oldValue, scope) {
+                    scope.$watch(
+                        function (scope) {return scope.aValue},
+                        function (newValue, oldValue, scope) {scope.counter ++;}
+                    )
+                });
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
+        it("compares based on value if enabled",function () {
+            scope.aValue=[1,2,3];
+            scope.counter=0;
+            scope.$watch(
+                function (scope) {return scope.aValue},
+                function (newValue, oldValue, scope) {
+                    scope.counter++;
+                },
+            true);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.aValue.push(4);
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+
+        });
+        it("correct handle the NaNs",function () {
+            scope.number=0/0;
+            scope.counter=0;
+            scope.$watch(
+                function (scope) {return scope.number},
+                function (newValue, oldValue, scope) {
+                    scope.counter++;
+                });
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
         })
 
 
